42b807ae4e9c80ed3fb2d4681a4da20c7d4c900d
tommyrharper <thomasrobertharper@gmail.com>
2022-03-30 21:32:35 +0100

    Add review detail view

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -7,6 +7,7 @@ from watchlist_app.api.views import (
     StreamPlatformAV,
     StreamPlatformDetailAV,
     ReviewList,
+    ReviewDetail,
 )
 
 ## method based urls
@@ -19,6 +20,11 @@ urlpatterns = [
     path("list/", WatchListAV.as_view(), name="movie-list"),
     path("<int:pk>/", WatchListDetailAV.as_view(), name="movie-detail"),
     path("stream/", StreamPlatformAV.as_view(), name="streamplatform-detail"),
-    path("stream/<int:pk>/", StreamPlatformDetailAV.as_view(), name="streamplatform-detail"),
-    path('review/', ReviewList.as_view(), name='review-list'),
+    path(
+        "stream/<int:pk>/",
+        StreamPlatformDetailAV.as_view(),
+        name="streamplatform-detail",
+    ),
+    path("review/", ReviewList.as_view(), name="review-list"),
+    path("review/<int:pk>/", ReviewDetail.as_view(), name="review-detail"),
 ]
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,12 +1,24 @@
 from rest_framework.response import Response
 from watchlist_app.models import WatchList, StreamPlatform, Review
-from watchlist_app.api.serializers import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
+from watchlist_app.api.serializers import (
+    WatchListSerializer,
+    StreamPlatformSerializer,
+    ReviewSerializer,
+)
 
 # from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework import mixins
 from rest_framework import generics
+
+
+class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class ReviewList(
